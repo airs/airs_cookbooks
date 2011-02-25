85c3b06aecdb03eb4c63132755168a06abce350e
Masakuni Kato <kato@airs.co.jp>
2011-02-25 18:20:44 +0900

    Add JSON attributes for test

--- mysql/recipes/remove.rb
+++ mysql/recipes/remove.rb
@@ -1,4 +1,4 @@
-bash "stop-mysql-server" do
+bash "setoff-auto-startup" do
   code "/bin/launchctl unload -w /Library/LaunchDaemons/org.macports.mysql5.plist"
   only_if "test -f /Library/LaunchDaemons/org.macports.mysql5.plist"
 end
@@ -6,4 +6,8 @@ end
 bash "uninstall-mysql" do
   code "port uninstall mysql5-server; port uninstall mysql5"
   only_if { node.platform == "mac_os_x" }
+end
+
+bash "remove-install-dir" do
+  code "rm -rf #{node.mysql.install_dir}"
 end
