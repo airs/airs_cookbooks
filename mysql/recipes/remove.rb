2ae3ad398e8bd2392017a5ededb72072abe91a38
Masakuni Kato <kato@airs.co.jp>
2011-02-25 16:57:28 +0900

    Change mysql uninstall from package to bash

--- mysql/recipes/remove.rb
+++ mysql/recipes/remove.rb
@@ -3,8 +3,7 @@ bash "stop-mysql-server" do
   only_if "test -f /Library/LaunchDaemons/org.macports.mysql5.plist"
 end
 
-%w(mysql5-server mysql5).each do |package_name|
-  package package_name do
-    action :remove
-  end
+bash "uninstall-mysql" do
+  code "port uninstall mysql5-server; port uninstall mysql5"
+  only_if { node.platform == "mac_os_x" }
 end
